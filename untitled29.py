b1e5c8d3ba6f09242255598fe71ff6b86b047c72
diegoglzz000 <a01613808@tec.mx>
2025-11-27 09:23:42 -0600

    Update untitled29.py

--- untitled29.py
+++ untitled29.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1vB_G5viu58DpclLXgJmYzGrk15jruHIQ
 """
 
-!pip install streamlit
 import numpy as np
 import streamlit as st
 import pandas as pd
